525e7204dbd9d15ec06c2226f6edd90b87b6e5e7
Sandra <sandra.iriandini@gmail.com>
2020-04-21 22:50:19 +0200

    Implement image upload hooks in cloudinary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const uploadImage = async (event) => {
+    const files = event.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "cancerleo");
+    setLoading(true);
+    const response = await fetch(
+      "https://api.cloudinary.com/v1_1/cancerleo/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+
+    const file = await response.json();
+
+    setImage(file.secure_url);
+    setLoading(false);
+  };
+
   return (
     <div className="App">
       <h1>Cloudinary Upload Image</h1>
+      <input
+        type="file"
+        name="file"
+        placeholder="Upload an image"
+        onChange={uploadImage}
+      />
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <img src={image} style={{ width: "300px" }} />
+      )}
     </div>
   );
 }
